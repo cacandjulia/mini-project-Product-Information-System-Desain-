6c1ce537eef403934e6e1a3393516b0befdecf79
caca nurul dwi julia <cacanuruld@gmail.com>
2026-09-21 19:12:05 +0700

    Add product data array in products.php
    Initialized a products array with multiple product entries including id, name, category, price, stock, and description.

---
+++ products.php
@@ -0,0 +1,37 @@
+<?php
+// Data Layer: Menampung data produk dalam multidimensional array
+$products = [
+    [
+        "id" => "PRD001",
+        "nama" => "Kopi Arabika",
+        "kategori" => "Minuman",
+        "harga" => 25000,
+        "stok" => 10,
+        "deskripsi" => "Kopi biji utuh kualitas super"
+    ],
+    [
+        "id" => "PRD002",
+        "nama" => "Teh Hijau Organic",
+        "kategori" => "Minuman",
+        "harga" => 15000,
+        "stok" => 2, // Stok kritis (<3)
+        "deskripsi" => "Teh hijau herbal kaya antioksidan"
+    ],
+    [
+        "id" => "PRD003",
+        "nama" => "Keripik Singkong",
+        "kategori" => "Makanan",
+        "harga" => 12000,
+        "stok" => 15,
+        "deskripsi" => "Camilan renyah pedas manis"
+    ],
+    [
+        "id" => "PRD004",
+        "nama" => "Roti Tawar",
+        "kategori" => "Makanan",
+        "harga" => 10000,
+        "stok" => 1, // Stok kritis (<3)
+        "deskripsi" => "Roti tawar gandum lembut"
+    ]
+];
+?>
